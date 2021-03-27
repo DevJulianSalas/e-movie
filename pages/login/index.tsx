ded9f64ff26ad6ad1cda14cfdb1e0b56dbdfc4f7
julian <6efilm.infobibliotecas@gmail.com>
2021-03-26 20:01:13 -0500

    added login screen

---
+++ pages/login/index.tsx
@@ -0,0 +1,24 @@
+import { React } from "react";
+import { Row, Col, Layout } from 'antd';
+
+import styles from './loginpage.module.scss'
+
+
+console.log(styles)
+
+const Login = () => {
+  return (
+    <Row justify="center" align="middle">
+      <Col span={12}>
+        <div className={styles.sliderLeft}>
+          <p>logo</p>
+        </div>
+      </Col>  
+      <Col span={12}>
+        <p>form</p>
+      </Col>
+    </Row>
+  )
+}
+
+export default Login;
