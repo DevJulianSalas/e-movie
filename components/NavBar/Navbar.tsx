54ee20e0b0cbc27309af38d0e0fdf9c21d1c61bb
julian <6efilm.infobibliotecas@gmail.com>
2021-03-26 20:00:52 -0500

    added login screen

--- components/NavBar/Navbar.tsx
+++ components/NavBar/Navbar.tsx
@@ -1,13 +1,31 @@
 import React,  { FC } from 'react';
 import { Layout } from 'antd';
 const { Header } = Layout;
+import { Button, Row, Col } from 'antd';
+import { useRouter } from 'next/router'
+
+
+import brandLogo from '../../public/vercel.svg'
+import Image from "next/image";
+
 //styles
 import styles from './NavBar.module.scss';
 
+
 export default function NavBar() {
+  const router = useRouter()
   return (
     <>
-      <Header className={styles.navbar}>Header</Header>
+      <Header className={styles.navbar}>
+        <Row  justify="start">
+          <Col span={22}>
+            <p>logo</p>
+          </Col>
+          <Col>
+          <Button  onClick={() => router.push('/login')} type="primary">Ingreso</Button>
+          </Col>
+        </Row>
+      </Header>
     </>
   )  
 }
--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+/// <reference types="next-images" />
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.gif';
+declare module '*.svg';
